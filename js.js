60e23419901aeb62e2d213176775a5842f9f3c7a
Leal <90978901+Lealw@users.noreply.github.com>
2022-01-07 01:33:02 -0300

    Adicionando arquivos
    Versão Final
    V1.0

---
+++ js.js
@@ -0,0 +1,46 @@
+function txt1()
+    {
+        alert("Como você conseguirá impactar essa geração se você continuar cometendo os mesmos erros? \nJá estamos fartos dessa autosabotagem. \nNão sei aonde você vai chegar se continuar vivendo assim, e creio que você não quer permanecer desse jeito para descobrir. \nTodos os dias você luta contra si mesmo, contra sua vontade e seus pensamentos. Você precisa deixar os erros pra trás, para se tornar melhor a cada dia mais. \n\nVocê quer ser diferente? \nQuer ser luz nesse mundo cheio de escuridão? \nSe a resposta for sim, então se movimente, pois você não conseguirá nada se não se dedicar. Receber ajuda de alguém é muito bom, mas o melhor é quando você pode ser a pessoa que ajuda. \nJesus está ao seu lado, então não desista, e se agarre ás oportunidades. \nMais um ano se iniciou, e se você quer que esse ano seja diferente dos outros, VOCÊ precisa ser diferente. \nMova-se. \n\nTexto: @debora.cacilda\nLíder: @agnaldoreis22\nPastor: @pablosilvalpa")
+        
+    }
+
+function txt2()
+    {
+        
+        alert("Quando Deus criou o ser humano Ele soprou sobre ele e o mesmo ficou cheio do Espírito Santo. Entretanto, por causa do pecado, Adão, o primeiro homem pecou e foi destituído da glória de Deus e consequentemente gerou filhos e filhas destituídos da glória de Deus, do Espírito Santo.\n\nDeus sabia que isso poderia acontecer, pois, criara o homem com liberdade de escolha e sabia que ele poderia escolher a desobediência e consequentemente a morte. Então, de antemão Deus também preparou o Salvador para entrar em ação caso o homem pecasse. Por causa da morte do Salvador, hoje, a glória de Deus, o Espírito Santo está sendo RESTITUÍDO aos homens e mulheres que o desejam.\n\nAssim como a destituição da glória de Deus aconteceu como fruto da vontade humana, da mesma forma acontece em relação aa restituição. Se você deseja ser restituído da glória de Deus, aceite a Jesus Cristo como seu Senhor e Salvador, pois, em tudo aquilo que Adão falhou, Ele, a nossa semelhança foi fiel; e por causa da sua morte o salário do pecado foi pago.\n\nVocê foi substituído lá na Cruz. Sem merecer, Jesus Cristo morreu a tua morte, para que, sem merecer você pudesse herdar a vida dEle. A vida de Cristo é aquela mesma que estava em Adão no Éden… é a Glória de Deus, o Espírito de Deus.")
+    }
+
+function txt3()
+    {
+        
+        alert("Você que está na caminhada com Cristo e ainda não descobriu qual é o seu chamado,não desista, não desanime, continue na caminhada. Porque é nesse percurso que vai te mostrar qual é o seu chamado, é nesse percurso que você vai descobrir qual é o motivo pelo qual você nasceu.")
+    }
+
+function txt4()
+    {
+        
+        alert("Aquele que diz que está na luz, e odeia a seu irmão, até agora está em trevas.\nAquele que ama a seu irmão está na luz, e nele não há escândalo.\nMas aquele que odeia a seu irmão está em trevas, e anda em trevas, e não sabe para onde deva ir; porque as trevas lhe cegaram os olhos.\n\n1 João 2:9-11")
+    }
+
+
+/*
+const header = document.querySelector("header");
+header.insertAdjacentHTML("afterbegin","<h1> Titulo </h1>"); 
+
+const main = document.querySelector("main");
+main.insertAdjacentHTML("afterbegin","<div id='arroz'></div>"); 
+main.insertAdjacentHTML("beforeend","<div id='feijao'></div>"); 
+
+const arroz = document.getElementById("arroz");
+arroz.insertAdjacentHTML("afterbegin","<h2> C1 </h2>"); 
+arroz.insertAdjacentHTML("beforeend","<p> C2 </p>")
+
+const feijao = document.getElementById("feijao");
+feijao.insertAdjacentHTML("afterbegin","<h2> D1 </h2>"); 
+feijao.insertAdjacentHTML("beforeend","<p> D2 </p>")
+
+const paragrafo = document.createElement('p');
+const txt = document.createTextNode("eu amo arroz e feijao")
+paragrafo.appendChild(txt)
+main.appendChild(paragrafo)
+*/
